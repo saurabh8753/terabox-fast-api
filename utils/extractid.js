29bd860f13828eee5bdbdb033234c5a3a7f67fec
Skzbots <130946713+saurabh8753@users.noreply.github.com>
2026-05-21 07:55:27 +0530

    Delete utils directory

--- utils/extractid.js
+++
@@ -1,7 +0,0 @@
-function extractShortUrl(url) {
-  const match = url.match(/\/s\/([a-zA-Z0-9_-]+)/);
-  if (!match) throw new Error("Invalid Terabox URL");
-  return match[1];
-}
-
-module.exports = extractShortUrl;
